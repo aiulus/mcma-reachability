e9068baf4ff29fcc6c471cfbb0945a92fd894829
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2025-04-16 17:27:15 +0200

    Adding another data-driven reachability analysis module

--- ddra/utilities/compLipConst.m
+++ ddra/utilities/compLipConst.m
@@ -3,13 +3,13 @@ function [gamma,L]= compLipConst(fun,U,R0,steps,initpoints,dim_x)
 
 totalsamples = initpoints*steps;
 %input random sample points
-for i = 1:totalsamples
+for i=1:totalsamples
     u(:,i) = randPointExtreme(U);
 end
 
 %get state trajectories
-index = 1;
-for j = 1:dim_x:initpoints*dim_x
+index=1;
+for j=1:dim_x:initpoints*dim_x
     x_free(j:j+dim_x-1,1) = randPoint(R0);
     for i=1:steps
         x_free(j:j+dim_x-1,i+1) = fun(x_free(j:j+dim_x-1,i),u(:,index));
@@ -31,52 +31,60 @@ for j=1:dim_x:initpoints*dim_x
         index_0 = index_0 +1;
     end
 end
-
-
-
-L(1) = [0];
-gamma(1) = 0;
-idx=1;
-for i=1:totalsamples
-    z1= [x_free_vec_0(:,i);u(:,i)];
-    f1= x_free_vec_1(idx,i);
-    for j=1:totalsamples
-        z2= [x_free_vec_0(:,j);u(:,j)];    
-        f2= x_free_vec_1(idx,j);
-        if z1~=z2
-            newnorm = norm(f1-f2)./norm(z1-z2);
-            newgamma = norm(z1-z2);
-            if newnorm > L(1)
-                L(1) = newnorm(1);
+% x_free_vec_1= normalize(x_free_vec_1);
+% x_free_vec_0 = normalize(x_free_vec_0);
+% u = normalize(u);
+normType=2;
+for idx=1:dim_x
+    L(idx) = [0];
+    gamma(idx) = 0;
+    for i=1:totalsamples
+        z1= [x_free_vec_0(idx,i);u(:,i)];
+        f1= x_free_vec_1(idx,i);
+        for j=1:totalsamples
+            z2= [x_free_vec_0(idx,j);u(:,j)];
+            f2= x_free_vec_1(idx,j);
+            if z1~=z2
+                newnorm = norm(f1-f2,normType)./norm(z1-z2,normType);
+                newgamma = norm(z1-z2,normType);
+                if newnorm > L(idx)
+                    L(idx) = newnorm;
+                end
+                if newgamma > gamma(idx)
+                    gamma(idx) = newgamma;
+                end
             end
-            if newgamma > gamma(1)
-                gamma(1) = newgamma;
-            end
-           
         end
     end
-end
-L(2) = [0];
-gamma(2) = 0;
-idx=2;
-for i=1:totalsamples
-    z1= [x_free_vec_0(:,i);u(:,i)];
-    f1= x_free_vec_1(idx,i);
-    for j=1:totalsamples
-        z2= [x_free_vec_0(:,j);u(:,j)];    
-        f2= x_free_vec_1(idx,j);
-        if z1~=z2
-            newnorm = norm(f1-f2)./norm(z1-z2);
-            newgamma = norm(z1-z2);
-            if newnorm > L(2)
-                L(2) = newnorm;
-            end           
-            if newgamma > gamma(2)
-                gamma(2) = newgamma;
-            end         
-        end
-    end
-end
 
 end
+end
 
+% L = 0;
+% gamma = 0;
+% for i=1:totalsamples
+%     z1= [x_free_vec_0(:,i);u(:,i)];
+%     f1= x_free_vec_1(:,i);
+%     for j=1:totalsamples
+%         z2= [x_free_vec_0(:,j);u(:,j)];    
+%         f2= x_free_vec_1(:,j);
+%         if z1~=z2
+%             newnorm = norm(f1-f2)./norm(z1-z2);
+%             newgamma = norm(z1-z2);
+%             if newnorm(1) > L(1)
+%                 L(1) = newnorm(1);
+%             end
+% %             if newnorm(2) > L(2)
+% %                 L(2) = newnorm(2);
+% %             end            
+%             if newgamma > gamma
+%                 gamma(1) = newgamma(1);
+%             end
+% %             if newgamma(2) > gamma
+% %                 gamma(2) = newgamma(2);
+% %            end            
+%         end
+%     end
+% end
+% eps(2)= L .* gamma;
+% end
