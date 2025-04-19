cbb69741ff45c556bcf0902145069b95aa0d77bf
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2025-04-19 13:35:51 +0200

    Modularizing mainZonoMPC

--- core/runZPC.m
+++ core/runZPC.m
@@ -1,8 +1,30 @@
 function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
     runZPC(sys_d, y0, intc, r_u, r_y, U, N, maxsteps, timestep_plot, Qy, Qu, W, V, AV, M_Sigma)
+    
+    %% PREALLOCATION STEPS
+    p = size(y0,1);
+    y_t = zeros(p, maxsteps + 1);
+    y_t_model = zeros(p, maxsteps + 1);
 
-    execTimeZPC=[];
-    execTimeRMPC=[];
+    uPred        = zeros(1, maxsteps);
+    uPred_model  = zeros(1, maxsteps);
+
+    YPred        = zeros(p, maxsteps + 1);
+    YPred_model  = zeros(p, maxsteps + 1);
+
+    execTimeZPC  = zeros(1, maxsteps);
+    execTimeRMPC = zeros(1, maxsteps);
+
+    Rplotall     = cell(1, maxsteps);
+
+    yt2ref       = zeros(1, maxsteps);
+    yt2ref_model = zeros(1, maxsteps);
+    
+    % For optional plotting at timestep_plot
+    R_N = cell(1, N+1);
+    R_N_int = cell(1, N+1);
+    y_N = cell(1, N+1);
+    u_N = cell(1, N);
 
     for k = 1:maxsteps
         if k == 1
@@ -18,12 +40,16 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
         alpha_u = sdpvar(1,N);
         sinf = sdpvar(5*ones(1,N+1),ones(1,N+1));
         ssup = sdpvar(5*ones(1,N+1),ones(1,N+1));
-        R={};
+        %R = {};
+        R = cell(1, N+1);
         R{1} = zonotope([y_t(:,k)]);
 
         %set the first constraint as y_t = current y
-        Constraints = [y_t(:,k) == y{1}]; %,...       
+        Constraints = y_t(:,k) == y{1}; %,...       
         
+        rightLimit = cell(1, N);
+        leftLimit = cell(1, N);
+
         for i = 1:N        
             genleni = size(R{i}.generators,2);
 
@@ -37,13 +63,15 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
     
             % convert R to interval        
             c = R{i+1}.c; % extract center
-            delta = sum(abs(R{i+1}.G),2);
+
+            %% TODO: find out why this is here
+            % delta = sum(abs(R{i+1}.G),2);
             
             delta = sum(abs(R{i+1}.Z),2) - abs(c);
             leftLimit{i} = c - delta; % determine left limit of the reahable set
             rightLimit{i} = c + delta; % determine right limit of the reahable set
             
-            %specify the constraints
+            % specify the constraints
             Constraints = [Constraints,...
                 u{i} == U.center + alpha_u(i) * U.generators,...
                 y{i+1} - sinf{i} == leftLimit{i},...
@@ -57,18 +85,26 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
                 ];
         end
         
-        %% cho0se the cost of ZPC
+        %% choose the cost of ZPC
         Cost=0;
         for i=1:N
             Cost = Cost + (y{i+1}-r_y)'*Qy*(y{i+1}-r_y)+ (u{i}-r_u)'*Qu*(u{i}-r_u);
         end
 
-        %solve ZPC
+        %% solve ZPC
         options = sdpsettings('verbose',0,'solver','mosek');
         tic
+
         Problem = optimize(Constraints,Cost,options);
-        execTimeZPC=[execTimeZPC,toc];
+        fprintf("Problem (step %d / %d): \n", k, maxsteps);
+        disp(Problem);
+
         Objective = double(Cost);
+        fprintf("Objective (step %d / %d): \n", k, maxsteps);
+        disp(Objective);
+
+        execTimeZPC=[execTimeZPC,toc];
+        
         uPred(k) = double(u{1});
         YPred(:,k+1) = double(y{2});
     
@@ -95,7 +131,7 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
         R{1} = zonotope([y_t_model(:,k)]);
         u_model = sdpvar(1*ones(1,N),ones(1,N));
         y_model = sdpvar(5*ones(1,N+1),ones(1,N+1));
-        Constraints = [y_t_model(:,k) == y_model{1}];
+        Constraints = y_t_model(:,k) == y_model{1};
 
         for i = 1:N
             %compute the reachable set for ZPC
@@ -108,7 +144,7 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
             card_zono = zonotope(dummy_cen, dummy_gen);
     
             ABcard = intervalMatrix(M_Sigma)* card_zono;
-            R{i+1} = zonotope([ABcard.center,[ABcard.generators,W.generators,V.generators,AV.generators]]);%A B * card_zono + W_sdp;
+            R{i+1} = zonotope([ABcard.center,[ABcard.generators,W.generators,V.generators,AV.generators]]); %A B * card_zono + W_sdp;
 
             %convert R to interval           
             c = R{i+1}.Z(:,1);  %extract center             
@@ -134,7 +170,7 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
             Cost_model = Cost_model + (y_model{i+1}-r_y)'*Qy*(y_model{i+1}-r_y)+ (u_model{i}-r_u)'*Qu*(u_model{i}-r_u);
         end
 
-        options = sdpsettings('verbose',0,'solver','mosek');
+        options = sdpsettings('verbose', 0, 'solver', 'mosek');
         tic
         Problem = optimize(Constraints,Cost_model,options);
         execTimeRMPC=[execTimeRMPC,toc];
@@ -142,7 +178,7 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
         uPred_model(k) = double(u_model{1});
         YPred_model(:,k+1) = double(y_model{2});    
         
-        % apply the optimal control input to the plant 
+        %% apply the optimal control input 
         w_point = randPoint(W);
         v_point = randPoint(V);
         y_t(:,k+1) = sys_d.A * y_t(:,k) + sys_d.B * uPred(k) + w_point +v_point - sys_d.A *v_point;
@@ -150,7 +186,9 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
         
         yt2ref(k) = norm(y_t(:,k)-r_y,2);
         yt2ref_model(k) = norm(y_t_model(:,k)-r_y,2);
-        halt = 1;
+
+        %% TODO: Find out why this is here
+        % halt = 1;
     end
 end
 
