2f5bd637f42a8320bcd402ec4a7809748b3bb1f5
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2025-04-19 14:00:00 +0200

    Modularizing runZPC

--- core/runZPC.m
+++ core/runZPC.m
@@ -28,88 +28,92 @@ function [uPred, uPred_model, y_t, y_t_model, execTimeZPC, execTimeRMPC] = ...
 
     for k = 1:maxsteps
         if k == 1
-            % set the initial output to y0
-            y_t(:,k) = y0;
-            y_t_model(:,k) = y0;
-            YPred(:,1) = y0;
-        end        
+            [y_t, y_t_model, YPred] = runZPCinitializeY0(y0, y_t, y_t_model, YPred, k);
+        end
+
+        [uPred(k), YPred(:,k+1), u, y, execTimeZPC(k), Rplotall{k}] = ...
+            solveZPC(y_t(:,k), sys_d, N, r_u, r_y, U, Qy, Qu, W, V, AV, intc, k, maxsteps);
+
         
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % sdpvar variables
-        u = sdpvar(1*ones(1,N),ones(1,N));
-        y = sdpvar(5*ones(1,N+1),ones(1,N+1));
-        alpha_u = sdpvar(1,N);
-        sinf = sdpvar(5*ones(1,N+1),ones(1,N+1));
-        ssup = sdpvar(5*ones(1,N+1),ones(1,N+1));
-        %R = {};
-        R = cell(1, N+1);
-        R{1} = zonotope([y_t(:,k)]);
+        %u = sdpvar(1*ones(1,N),ones(1,N));
+        %y = sdpvar(5*ones(1,N+1),ones(1,N+1));
+        %alpha_u = sdpvar(1,N);
+        %sinf = sdpvar(5*ones(1,N+1),ones(1,N+1));
+        %ssup = sdpvar(5*ones(1,N+1),ones(1,N+1));
+        %%%R = {};
+        %R = cell(1, N+1);
+        %R{1} = zonotope([y_t(:,k)]);
 
         %set the first constraint as y_t = current y
-        Constraints = y_t(:,k) == y{1}; %,...       
+        %Constraints = y_t(:,k) == y{1}; %,...       
         
-        rightLimit = cell(1, N);
-        leftLimit = cell(1, N);
+        %rightLimit = cell(1, N);
+        %leftLimit = cell(1, N);
 
-        for i = 1:N        
-            genleni = size(R{i}.generators,2);
+        %for i = 1:N        
+            %genleni = size(R{i}.generators,2);
 
-            placeholder_u = r_u;  % safe numeric value
-            dummy_cen = [R{i}.center; placeholder_u];
-            dummy_gen = [R{i}.generators; zeros(1, genleni)];
-            card_zono = zonotope(dummy_cen, dummy_gen);    
+            %placeholder_u = r_u;  % safe numeric value
+            %dummy_cen = [R{i}.center; placeholder_u];
+            %dummy_gen = [R{i}.generators; zeros(1, genleni)];
+            %card_zono = zonotope(dummy_cen, dummy_gen);    
             
-            ABcard = [sys_d.A , sys_d.B]* card_zono;
-            R{i+1} = zonotope([ABcard.center,[ABcard.generators,W.generators,V.generators,AV.generators]]); %AB * card_zono + W_sdp;     
+            %ABcard = [sys_d.A , sys_d.B]* card_zono;
+            %R{i+1} = zonotope([ABcard.center,[ABcard.generators,W.generators,V.generators,AV.generators]]); %AB * card_zono + W_sdp;     
     
             % convert R to interval        
-            c = R{i+1}.c; % extract center
+            %c = R{i+1}.c; % extract center
 
             %% TODO: find out why this is here
-            % delta = sum(abs(R{i+1}.G),2);
+            %% delta = sum(abs(R{i+1}.G),2);
             
-            delta = sum(abs(R{i+1}.Z),2) - abs(c);
-            leftLimit{i} = c - delta; % determine left limit of the reahable set
-            rightLimit{i} = c + delta; % determine right limit of the reahable set
+            %delta = sum(abs(R{i+1}.Z),2) - abs(c);
+            %leftLimit{i} = c - delta; % determine left limit of the reahable set
+            %rightLimit{i} = c + delta; % determine right limit of the reahable set
             
             % specify the constraints
-            Constraints = [Constraints,...
-                u{i} == U.center + alpha_u(i) * U.generators,...
-                y{i+1} - sinf{i} == leftLimit{i},...
-                y{i+1} + ssup{i} == rightLimit{i},...
-                y{i+1} - sinf{i} >= intc.inf,...
-                y{i+1} + ssup{i} <= intc.sup,...
-                sinf{i} >= zeros(5,1),...
-                ssup{i} >= zeros(5,1),...
-                alpha_u(i) <= 1 , ...
-                alpha_u(i) >= -1, ...
-                ];
-        end
+        %    Constraints = [Constraints,...
+        %        u{i} == U.center + alpha_u(i) * U.generators,...
+        %        y{i+1} - sinf{i} == leftLimit{i},...
+        %        y{i+1} + ssup{i} == rightLimit{i},...
+        %        y{i+1} - sinf{i} >= intc.inf,...
+        %        y{i+1} + ssup{i} <= intc.sup,...
+        %        sinf{i} >= zeros(5,1),...
+        %        ssup{i} >= zeros(5,1),...
+        %        alpha_u(i) <= 1 , ...
+        %        alpha_u(i) >= -1, ...
+        %        ];
+        %end
         
         %% choose the cost of ZPC
-        Cost=0;
-        for i=1:N
-            Cost = Cost + (y{i+1}-r_y)'*Qy*(y{i+1}-r_y)+ (u{i}-r_u)'*Qu*(u{i}-r_u);
-        end
+        %Cost=0;
+        %for i=1:N
+        %    Cost = Cost + (y{i+1}-r_y)'*Qy*(y{i+1}-r_y)+ (u{i}-r_u)'*Qu*(u{i}-r_u);
+        %end
 
         %% solve ZPC
-        options = sdpsettings('verbose',0,'solver','mosek');
-        tic
+        %options = sdpsettings('verbose',0,'solver','mosek');
+        %tic
 
-        Problem = optimize(Constraints,Cost,options);
-        fprintf("Problem (step %d / %d): \n", k, maxsteps);
-        disp(Problem);
+        %Problem = optimize(Constraints,Cost,options);
+        %fprintf("Problem (step %d / %d): \n", k, maxsteps);
+        %disp(Problem);
 
-        Objective = double(Cost);
-        fprintf("Objective (step %d / %d): \n", k, maxsteps);
-        disp(Objective);
+        %Objective = double(Cost);
+        %fprintf("Objective (step %d / %d): \n", k, maxsteps);
+        %disp(Objective);
 
-        execTimeZPC=[execTimeZPC,toc];
+        %execTimeZPC=[execTimeZPC,toc];
         
-        uPred(k) = double(u{1});
-        YPred(:,k+1) = double(y{2});
+        %uPred(k) = double(u{1});
+        %YPred(:,k+1) = double(y{2});
     
         %% save for plotting
-        Rplotall{k} = interval(zonotope(R{2}.c, R{2}.G));
+        %Rplotall{k} = interval(zonotope(R{2}.c, R{2}.G));
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
         %%  ploting
         if timestep_plot == k
             for i =1:N+1
