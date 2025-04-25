3dff7170b6b2b3f1f293e379df0a6a646aee77ec
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2025-04-25 15:15:01 +0200

    Restructuring repo into data-driven vs. conformance-based branches

--- ddpc/contDynamics/@linParamSys/LinParamWrapper_initReach.m
+++
@@ -1,6 +0,0 @@
-function varargout = LinParamWrapper_initReach(varargin)
-%wrapper for initReach function from LinParamSys
-
-%------------- BEGIN CODE --------------
-[varargout{1:nargout}] = initReach(varargin{:});
-%------------- END OF CODE --------------
